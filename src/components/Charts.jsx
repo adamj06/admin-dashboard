46d8888e81bc3eeaaec9f3b07bf8ea891b4209d1
adamj06 <66421205+adamj06@users.noreply.github.com>
2021-01-29 13:46:08 +0000

    Add more sales charts

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -2,24 +2,67 @@ import React from "react";
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import sales from "./data/sales";
 
-const formatXAxis = (tickItem) => {
-  return tickItem.toLocaleDateString();
-}
-
 function Charts() {
+  function formatXAxis(tickItem) {
+    try {
+      return tickItem.toLocaleDateString();
+    }
+    catch (err) {
+      return "No Data";
+    }
+  }
+
+  function filterData(data, days) {
+    return data.filter(item => item.date.getTime() >= (new Date().getTime() - (86400000 * days)));
+  }
+
+  // Define data
+  const sales90 = filterData(sales, 90);
+  const sales30 = filterData(sales, 30);
+
   return(
-    <div className="chart-container">
-      <h3>Sales (All)</h3>
-      <div className="recharts-wrapper">
-      <ResponsiveContainer width="95%" height={400}>
-        <LineChart width={600} height={300} data={sales}>
-          <Line type="monotone" dataKey="amount" stroke="#8884d8" />
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <XAxis dataKey="date" tickFormatter={formatXAxis}/>
-          <YAxis />
-          <Tooltip />
-        </LineChart>
-      </ResponsiveContainer>
+    <div>
+      <div className="chart-container">
+        <h3>Sales (Lifetime)</h3>
+        <div className="recharts-wrapper">
+        <ResponsiveContainer width="95%" height={400}>
+          <LineChart width={600} height={300} data={sales}>
+            <Line type="monotone" dataKey="amount" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="date" tickFormatter={formatXAxis}/>
+            <YAxis />
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+        </div>
+      </div>
+      <div className="chart-container">
+        <h3>Sales (90 Days)</h3>
+        <div className="recharts-wrapper">
+        <ResponsiveContainer width="95%" height={400}>
+          <LineChart width={600} height={300} data={sales90}>
+            <Line type="monotone" dataKey="amount" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="date" tickFormatter={formatXAxis}/>
+            <YAxis />
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+        </div>
+      </div>
+      <div className="chart-container">
+        <h3>Sales (30 Days)</h3>
+        <div className="recharts-wrapper">
+        <ResponsiveContainer width="95%" height={400}>
+          <LineChart width={600} height={300} data={sales30}>
+            <Line type="monotone" dataKey="amount" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="date" tickFormatter={formatXAxis}/>
+            <YAxis />
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+        </div>
       </div>
     </div>
   );
